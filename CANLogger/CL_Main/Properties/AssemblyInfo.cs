55283294455a017c903ef63dea111f4e560dd7d6
letheascetic <yizhifight@163.com>
2018-12-03 14:28:39 +0800

    update about

--- CANLogger/CL_Main/Properties/AssemblyInfo.cs
+++ CANLogger/CL_Main/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("CAN Logger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A CAN Bus Tool For WIN OS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Torch Imaging Technology Co., Ltd.")]
 [assembly: AssemblyProduct("CAN Logger")]
